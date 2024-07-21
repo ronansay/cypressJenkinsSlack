cccd8cfbeb57ce3fcd60e9757961f54a2425fd4c
ronansay <ronansays@gmail.com>
2024-07-21 23:40:24 +0200

    added docker file

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  experimentalStudio: false,
+  experimentalSessionSupport: false, 
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
+    },
+    reporterOptions: {
+      reporterDir: 'cypress-report',
+      charts: true,
+      quiet: true,
+      overwrite: false,
+      html: false,
+      json: true,
+      embeddedScreenshots: true
     },
   },
 });
